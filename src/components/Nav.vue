0757ba730fbbeb335191e005245abc2d6b72208a
NielsV8 <niels.verdickt@outlook.com>
2023-05-04 22:12:07 +0200

    feat: add nav icons + extra routes

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,25 +1,45 @@
 <script setup>
 import router from '../router/router';
+
+//add classlist active to current view
+document.querySelectorAll(".nav__item").forEach((ele) =>
+  ele.addEventListener("click", function (event) {
+    event.preventDefault();
+    document
+      .querySelectorAll(".nav__item")
+      .forEach((ele) => ele.classList.remove("active"));
+    this.classList.add("active")
+  })
+);
+
+
+
+
 </script>
 
 <template>
     <nav>
         <div class="nav__menu">
             <img class="logo__nav" src="src/assets/EmblemWhite.svg" alt="">
-            <div class="nav__item" active-class="active">
-            <router-link exact to="/home">Home</router-link>
+            <div class="nav__item">
+                <img class="emblem" src="src/assets/HomeEmblem.svg" alt="home">
+                <router-link exact to="/home">Home</router-link>
             </div>
-            <div class="nav__item" active-class="active">
-            <router-link exact to="/videos">Video's</router-link>
+            <div class="nav__item">
+                <img class="emblem" src="src/assets/VideosEmblem.svg" alt="videos">
+                <router-link exact to="/videos">Video's</router-link>
             </div>
-            <div class="nav__item" active-class="active">
-            <router-link exact to="/velden">Velden</router-link>
+            <div class="nav__item">
+                <img class="emblem" src="src/assets/VeldenEmblem.svg" alt="velden">
+                <router-link exact to="/velden">Velden</router-link>
             </div>
-            <div class="nav__item" active-class="active">
-            <router-link exact to="/inbox">Inbox</router-link>
+            <div class="nav__item">
+                <img class="emblem" src="src/assets/MailEmblem.svg" alt="inbox">
+                <router-link exact to="/inbox">Inbox</router-link>
             </div>
-            <div class="nav__item" active-class="active">
-            <router-link exact to="/profiel">Profiel</router-link>
+            <div class="nav__item">
+                <img class="emblem" src="src/assets/ProfileEmblem.svg" alt="profile">
+                <router-link exact to="/profiel">Profiel</router-link>
             </div>
         </div>
     </nav>
@@ -31,6 +51,8 @@ import router from '../router/router';
 nav {
     width: 30%;
     height: 100%;
+    bottom: 0;
+    left: 0;
     background-color: var(--deepSeaGreen800);
     overflow: hidden;
     position: fixed;
@@ -45,19 +67,30 @@ nav {
 }
 
 .nav__item {
-    text-align: center;
+    display: flex;
+    align-items: center;
     color: var(--offWhite);
-    margin: 10px;
-    padding: 10px;
+    margin: 16px;
 }
 
-.active{
-    display: flex;
+.active {
     background-color: var(--offWhite);
     color: var(--deepSeaGreen800);
-    padding: 10px 40px;
+    padding: 10px 30px;
     border-radius: 10px;
     font-weight: bold;
+}
+
+.emblem{
+    width: 20px;
+    margin-right: 6px;
+    filter: brightness(0) saturate(100%)invert(99%) sepia(1%) saturate(7485%) hue-rotate(173deg) brightness(127%) contrast(95%);
+}
+.active .emblem{
+    width: 20px;
+    margin-right: 6px;
+    filter: brightness(0) saturate(100%) invert(29%) sepia(18%) saturate(2138%) hue-rotate(118deg) brightness(90%) contrast(94%);
+
 }
 
 .logo__nav {
@@ -65,6 +98,7 @@ nav {
     margin-top: 20px;
     margin-bottom: 80px;
 }
+
 
 /* Desktop */
 @media (min-width: 992px) {}
---
+++ src/views/Profiel.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+</template>
+
+<style scoped>
+/*  mobile */
+
+/*  desktop */
+@media (min-width: 992px) {}
+</style>
